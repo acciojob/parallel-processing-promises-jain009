a8cfaf29091c1cb4f87dc460958bffed10dbde0c
Ankit jain <83234209+jain009@users.noreply.github.com>
2025-02-09 15:07:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,38 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+
+funnction downloadImage(url){
+	return new Promise((resolve , reject) () => {
+		const img = new Image();
+		img.src  = url;
+
+		img.onload = () => resolve(img);
+		img.onerror () => reject(`Failed to load image ${url}`);
+			
+	
+	};
+	)
+	 function downloadImage(imageUrls){
+		 const outputDiv = document.getElementById("output");
+		 const errorDiv = document.getElelmentById("error");
+		 const loadingDiv = document.getElementById("loading");
+
+		 loadingDiv.style.display ="block";
+		 errorDiv.textContent = "";
+		 outputDiv.ineerHTML ="";
+		 Promise.all(imageUrls.map(downloadImage))
+		 .then(images => {
+			 loadingDiv.style.dispaly ="none";
+			 images.forEach(img => outputDiv.appendChild(img));
+		 })
+		 .catch(error => {
+			 laodingDiv.style.display ="none";
+			 errorDiv.textContent = error;
+		 });
+	 }
+}
+
+// Call the function to download images
+downloadImages(imageUrls);
